35b622b95d12c1b446ced6400338a098673022a0
Aline <alinenoronhadev@gmail.com>
2025-05-19 20:34:59 -0300

    Site - ADS

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('msgBtn').addEventListener('click', function() {
+    const msgArea = document.getElementById('msgArea');
+    msgArea.textContent = 'Bem-vindo ao meu site! Estou inciando e adoraria compartilhar minhas preferencias com vocês.';
+});
